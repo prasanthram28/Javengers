157b78d3b5e3af1c7977c4319d9e1987026c20df
tvchandu07 <tvchandu777@gmail.com>
2026-08-28 18:24:36 +0530

    feat: add prime number program

---
+++ src/com/Javengers/java/PrimeNumbers.java
@@ -0,0 +1,21 @@
+package com.Javengers.java;
+
+public class PrimeNumbers{
+	public static void main(String[] args) {
+
+        for (int num = 2; num <= 100; num++) {
+            int count = 0;
+
+            for (int i = 1; i <= num; i++) {
+                if (num % i == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                System.out.print(num+" ");
+            }
+        }
+    }
+	
+}
